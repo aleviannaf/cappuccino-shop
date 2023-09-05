ab4216a8f91d721af6ffe17a56c26b57cb4cf553
aleviannaf <alexandrevianna511@gmail.com>
2023-09-05 11:49:57 -0400

    feat:  struct page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
+"use client"
+
+import { FilterBar } from '@/components/filter-bar'
+import styles from './page.module.css'
+import { styled } from 'styled-components'
+import { ProductList } from '@/components/products-list'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const PageWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 export default function Home() {
+const client = new QueryClient()
   return (
-    <main>
-    </main>
+    <QueryClientProvider client={client}>
+      <PageWrapper className={styles.main}>
+        <FilterBar />
+        <ProductList />
+      </PageWrapper>
+    </QueryClientProvider>
   )
 }
