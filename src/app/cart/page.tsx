0c08d4c7f07d8df5b868d6bd99b84fd11cc26440
aleviannaf <alexandrevianna511@gmail.com>
2023-09-13 23:37:39 -0400

    fix: fixing cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -11,7 +11,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     flex-direction: column;
     gap: 32px;
 
